7e52765762aaf5540b32d4b646d0e19dd98f5e1e
Riya Dhariwal <rdhariwa@sfu.ca>
2020-02-14 11:10:50 -0800

    Fixed the hyperlink layout.

--- app/src/main/java/com/ramneet/zombieseeker/HelpScreen.java
+++ app/src/main/java/com/ramneet/zombieseeker/HelpScreen.java
@@ -28,24 +28,22 @@ public class HelpScreen extends AppCompatActivity {
         authorsHyperlink.setText(Text);
 
         TextView citationOneHyperlink = findViewById(R.id.citation);
-        Text = Html.fromHtml("<a href='https://i.pinimg.com/originals/5c/cf/da/5ccfdaddef13b6dcd7122f20e6f551f1.gif'>Zombie GIF</a> <br />\n" +
-                "<a href='https://cdn.hipwallpaper.com/i/31/45/V2zRmh.jpg'>Grey Background Image <br /> \n" +
-                "<a href='https://cdn.imgbin.com/12/21/25/imgbin-cartoon-zombie-zombie-KnZgwiBDaSSM4EHi72XqdSCU3.jpg'>Green Zombie</a>");
+        Text = Html.fromHtml("<a href='https://i.pinimg.com/originals/5c/cf/da/5ccfdaddef13b6dcd7122f20e6f551f1.gif'>Zombie GIF</a>");
 
         citationOneHyperlink.setMovementMethod(LinkMovementMethod.getInstance());
         citationOneHyperlink.setText(Text);
 
-//        TextView citationTwoHyperlink = findViewById(R.id.citation2);
-//        Text = Html.fromHtml( "<a href='https://cdn.hipwallpaper.com/i/31/45/V2zRmh.jpg'>Grey Background Image</a>");
-//
-//        citationTwoHyperlink.setMovementMethod(LinkMovementMethod.getInstance());
-//        citationTwoHyperlink.setText(Text);
-//
-//        TextView citationThreeHyperlink = findViewById(R.id.citation3);
-//        Text = Html.fromHtml( "<a href='https://cdn.imgbin.com/12/21/25/imgbin-cartoon-zombie-zombie-KnZgwiBDaSSM4EHi72XqdSCU3.jpg'>Green Zombie</a>");
-//
-//        citationThreeHyperlink.setMovementMethod(LinkMovementMethod.getInstance());
-//        citationThreeHyperlink.setText(Text);
+        TextView citationTwoHyperlink = findViewById(R.id.citation2);
+        Text = Html.fromHtml( "<a href='https://cdn.hipwallpaper.com/i/31/45/V2zRmh.jpg'>Grey Background Image</a>");
+
+        citationTwoHyperlink.setMovementMethod(LinkMovementMethod.getInstance());
+        citationTwoHyperlink.setText(Text);
+
+        TextView citationThreeHyperlink = findViewById(R.id.citation3);
+        Text = Html.fromHtml( "<a href='https://cdn.imgbin.com/12/21/25/imgbin-cartoon-zombie-zombie-KnZgwiBDaSSM4EHi72XqdSCU3.jpg'>Green Zombie</a>");
+
+        citationThreeHyperlink.setMovementMethod(LinkMovementMethod.getInstance());
+        citationThreeHyperlink.setText(Text);
 
 //        TextView citationOneHyperlink = findViewById(R.id.citation1);
 //        Text = Html.fromHtml("Zombie GIF link  <br />" +
